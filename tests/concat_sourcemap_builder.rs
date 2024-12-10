ea68f01f395da63b7caaad4dcdb4257b57216168
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-12-10 15:25:18 +0900

    refactor: minor

--- tests/concat_sourcemap_builder.rs
+++ tests/concat_sourcemap_builder.rs
@@ -11,34 +11,21 @@ fn concat_sourcemap_builder_with_empty() {
 
     let mut builder = ConcatSourceMapBuilder::default();
     let mut source = String::new();
-    {
-        let js = fs::read_to_string(dir.join("dep1.js")).unwrap();
-        let js_map = fs::read_to_string(dir.join("dep1.js.map")).unwrap();
-        let sourcemap = SourceMap::from_json_string(&js_map).unwrap();
-        builder.add_sourcemap(&sourcemap, source.lines().count() as u32);
-        source.push_str(&js);
-    }
-    {
-        // this soucemap has { mappings: '' }
-        let js = fs::read_to_string(dir.join("dep2.js")).unwrap();
-        let js_map = fs::read_to_string(dir.join("dep2.js.map")).unwrap();
-        let sourcemap = SourceMap::from_json_string(&js_map).unwrap();
-        builder.add_sourcemap(&sourcemap, source.lines().count() as u32);
-        source.push_str(&js);
-    }
-    {
-        let js = fs::read_to_string(dir.join("dep3.js")).unwrap();
-        let js_map = fs::read_to_string(dir.join("dep3.js.map")).unwrap();
+
+    // dep2.js.map has { mappings: "" }
+    for filename in ["dep1.js", "dep2.js", "dep3.js"] {
+        let js = fs::read_to_string(dir.join(filename)).unwrap();
+        let js_map = fs::read_to_string(dir.join(filename).with_extension("js.map")).unwrap();
         let sourcemap = SourceMap::from_json_string(&js_map).unwrap();
         builder.add_sourcemap(&sourcemap, source.lines().count() as u32);
         source.push_str(&js);
     }
 
     let sourcemap = builder.into_sourcemap();
-    // encode and decode to test token chunk serialization
+    // encode and decode back to test token chunk serialization
     let sourcemap = SourceMap::from_json(sourcemap.to_json()).unwrap();
+
     let visualizer = SourcemapVisualizer::new(&source, &sourcemap);
     let visualizer_text = visualizer.into_visualizer_text();
-    let visualizer_text = visualizer_text.replace("\\r\\n", "\\n");
     insta::assert_snapshot!("empty", visualizer_text);
 }
